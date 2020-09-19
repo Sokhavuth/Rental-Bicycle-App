560e9d9b6fdca0c028c909d0c0dd2b7040e9af51
Sokhavuth TIN <vuthdevelop@gmail.com>
2020-09-19 18:52:42 +0700

    Make it better

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from public import setup
   
 @route('/')
 def main():
+  config.kargs['message'] = ""
   return index.render(**config.kargs)
   
 if 'DYNO' in os.environ:
   run(host='0.0.0.0', port=os.environ.get('PORT', 9000))
 else: 
   run(host='localhost', port=9000, debug=True, reloader=True)
-  
